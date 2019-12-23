3951b09257d3cc6a309dbb08122b672eb4a70061
itoshin128 <itoshin128@gmail.com>
2019-12-23 20:55:23 +0900

    db migrate shipping address

--- db/migrate/20191223114848_create_shipping_addresses.rb
+++ db/migrate/20191223114848_create_shipping_addresses.rb
@@ -1,7 +1,12 @@
 class CreateShippingAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :shipping_addresses do |t|
-
+      t.integer :postal_code,                     null: false   
+      t.string :address,                          null: false
+      t.string :building,                         null: false
+      t.string :prefecture,                       null: false
+      t.integer :telephone,                       null: false
+      t.references :user,                         foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_085604) do
+ActiveRecord::Schema.define(version: 2019_12_23_114848) do
+
+  create_table "shipping_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "postal_code", null: false
+    t.string "address", null: false
+    t.string "building", null: false
+    t.string "prefecture", null: false
+    t.integer "telephone", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shipping_addresses_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_12_20_085604) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shipping_addresses", "users"
 end
