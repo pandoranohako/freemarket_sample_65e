0969ae5eb6047bfdc28b2e699899005787c601ff
itoshin128 <itoshin128@gmail.com>
2019-12-26 16:20:25 +0900

    for adding validation

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,10 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :user, optional: true
+
+  #バリデーション設定     
+  validates :postal_code, presence: true, length: { maximum: 7 }, format: { with: VALID_POSTAL_CODE, message: '有効ではありません。' }, on: :validates_step3
+  validates :address, presence: true, on: :validates_step3
+  validates :prefecture, presence: true, on: :validates_step3
+  validates :building, length: { maximum: 100 }, on: :validates_step3
+  validates :telephone, format: { with: VALID_PHONE_REGEX, message: '有効ではありません。'}, allow_blank: true, on: :validates_step3
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,46 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  #バリデーション設定       
-  validates :name, presence: true
 
   #アソシエーション
   has_one  :shipping_address, dependent: :destroy
+
+  #バリデーション設定   
+  # user_registration1入力項目    
+  validates :name, presence: true, length: { maximum: 20 }, on: :validates_step1
+  validates :email, presence: true, uniqueness: true,  format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*[a-zA-Z]+\z/ }, on: :validates_step1
+  validates :password, presence: true, length: { in: 6..128 },  format: { with: /\A(?=.*[^\d])+/ }, on: :validates_step1
+  validates :family_name, presence: true, length: { maximum: 20 }, on: :validates_step1
+  validates :first_name, presence: true, length: { maximum: 20 }, on: :validates_step1
+  validates :family_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'カタカナで入力して下さい。'}, on: :validates_step1
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'カタカナで入力して下さい。'}, on: :validates_step1
+  validates :birthday_yyyy, presence: true, on: :validates_step1
+  validates :birthday_mm, presence: true, on: :validates_step1
+  validates :birthday_dd, presence: true, on: :validates_step1  
+
+  # user_registration2入力項目
+  validates :telephone, presence: true, format: { with: VALID_PHONE_REGEX, message: '有効ではありません。'}, on: :validates_step2
+
+  # user_registration3入力項目
+  validates :family_name, presence: true, length: { maximum: 20 }, on: :validates_step3
+  validates :first_name, presence: true, length: { maximum: 20 }, on: :validates_step3
+  validates :family_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'カタカナで入力して下さい。'}, on: :validates_step3
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: VALID_KATAKANA_REGEX, message: 'カタカナで入力して下さい。'}, on: :validates_step3
+
+  # 下記の記述が無いと、devise機能によりemailとpasswordのバリデーションが抜けられない
+  protected
+  
+  def password_required?
+    # user_registration1のときだけバリデーションがかかるように条件分岐
+    if validation_context == :user_registration1
+      !persisted? || !password.nil? || !password_confirmation.nil?
+    end
+  end
+
+  def email_required?
+    # user_registration1のときだけバリデーションがかかるように条件分岐
+    if validation_context == :user_registration1
+      true
+    end
+  end
 end
