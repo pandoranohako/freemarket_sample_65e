66ff5b29963bc13682e9eba4e6e2b56f22f08b29
akki0083 <fighters.nkt@gmail.com>
2019-12-29 14:39:03 +0900

    readmeに従いマイグレーションファイルを編集

--- db/migrate/20191229044350_create_products.rb
+++ db/migrate/20191229044350_create_products.rb
@@ -2,6 +2,20 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
 
+      t.string     :name,          null: false
+      t.text       :description,   null: false
+      t.string     :condition,     null: false
+      t.string     :shipping_fee,  null: false
+      t.string     :shipping_how,  null: false
+      t.string     :shipping_from, null: false
+      t.string     :shipping_date, null: false
+      t.integer    :price,         null: false
+      t.integer    :likes_count,   default: 0
+      t.references :user_id,       foregin_key: true
+      t.references :customer_id,   foregin_key: true
+      t.references :category_id,   foregin_key: true
+      t.references :size_id,       foregin_key: true
+      t.references :brand_id,      foregin_key: true
 
       t.timestamps
     end
