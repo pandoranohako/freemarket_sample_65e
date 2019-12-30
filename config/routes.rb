1673122eaf1afc05fc517895ea0df5a5e854f31f
pandoranohako <m.o_5963@icloud.com>
2019-12-30 17:54:21 +0900

    ログインページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #マイページ
-  root "mypage#index"
-end
+  root to: "mypage#index"
+
+  resources :mypage, only: [:index, :new] do
+      get 'address'
+      get 'complete'
+      get 'login'
+      get 'member'
+      get 'payment'
+      get 'telephone'
+    end
+  end
