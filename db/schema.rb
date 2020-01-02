da547b19d0891ffb8c0b73a7ce8820adc06ec485
akki0083 <fighters.nkt@gmail.com>
2020-01-02 17:05:56 +0900

    imageカラムにnull制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_12_29_065439) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image"
+    t.string "image", null: false
     t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
