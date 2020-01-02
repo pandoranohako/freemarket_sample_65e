993b377b77c54f7062098c26f5f6162f2028cd0b
akki0083 <fighters.nkt@gmail.com>
2020-01-02 16:45:37 +0900

    factorybot編集

---
+++ spec/factories/images.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+
+  factory :image do
+    image              {"hoge/hoge"}
+  end
+
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+
+  factory :product do
+    name                  {"hoge"}
+    description           {"hoge"}
+    condition             {"hoge"}
+    shipping_fee          {"hoge"}
+    shipping_how          {"hoge"}
+    shipping_from         {"hoge"}
+    shipping_date         {"hoge"}
+    price                 {1000}
+    likes_count           {0}
+  end
+
+end
