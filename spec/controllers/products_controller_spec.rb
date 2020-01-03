5d5cc1fdf0e483220ad82e72473f83b22c7d36db
akki0083 <fighters.nkt@gmail.com>
2020-01-03 16:12:53 +0900

    imagesテーブルのデータが全てインスタンス変数に含まれているかテスト

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe ProductsController, type: :controller do
       get :index
       expect(assigns(:products)).to match(products.sort{ |a, b| b.created_at <=> a.created_at } )
     end
+
+    it "populates an array of all images" do
+      images = FactoryBot.create_list(:image, 3) 
+      get :index
+      expect(assigns(:images)).to match(images)
+    end
+
     it "renders the :index template" do
       get :index
       expect(response).to render_template :index
