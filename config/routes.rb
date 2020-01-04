4a8a5414dbfcc8b24eea9d88ac40078d6a174426
akki0083 <fighters.nkt@gmail.com>
2020-01-04 10:32:18 +0900

    商品出品ページへのルーティングを設定
    newアクションを指定

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #マイページ
   root "products#index"
+  resources :products, only: [:new] 
+
   resources :mypage do [:index]
     collection do
       get 'profile'         #プロフィール
     end
   end
-
 
   resources :mypage, only: [:index, :new] do
       get 'address'
@@ -18,6 +19,5 @@ Rails.application.routes.draw do
       get 'member'
       get 'payment'
       get 'telephone'
-    end
   end
-
+end
