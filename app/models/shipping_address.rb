0b9fcafd4b0ade7db633033615906596b7a3de0f
itoshin128 <itoshin128@gmail.com>
2020-01-07 19:44:46 +0900

    バリデーションおよびアソシエーションの設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,10 @@
 class ShippingAddress < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  #バリデーション設定     
+  validates :postal_code, presence: true, length: { maximum: 7 }
+  validates :address, presence: true
+  validates :prefecture, presence: true
+  validates :building, length: { maximum: 100 }
+  validates :telephone, allow_blank: true
 end
