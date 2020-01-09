3d936a262238d0fcca49da00cf275020da3fe713
akki0083 <fighters.nkt@gmail.com>
2020-01-09 21:49:59 +0900

    cardsテーブル再度マイグレーション
    マイグレーションファイルを修正したが、必要なかった。

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_01_07_050316) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
     t.string "customer_id", null: false
     t.string "card_id", null: false
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
