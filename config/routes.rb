c0b4969bd7d2a12f5a12f43df566e4b20d5e910b
itoshin128 <itoshin128@gmail.com>
2020-01-09 12:07:52 +0900

    新規登録入口ページのルーティング、コントローラ、ビューの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
 
   devise_scope :user do
+    get 'signup', to: 'users/registrations#index'
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
