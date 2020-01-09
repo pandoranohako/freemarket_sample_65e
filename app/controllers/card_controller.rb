b794056234be727a9ba27329a770ddbcf58b690f
akki0083 <fighters.nkt@gmail.com>
2020-01-09 22:05:48 +0900

    payjpにcard情報を記録させる

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -7,4 +7,16 @@ class CardController < ApplicationController
     card = Card.where(user_id: 1) #1をcurrent_user.idとしたい
     redirect_to action: "show" if card.exists?
   end
+  def pay #payjpとCardのデータベース作成を実施します。
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key) #creddential.yml.enb から呼び出せないので仮置き
+    if params['payjp-token'].blank?
+      redirect_to action: "new"
+    else
+      customer = Payjp::Customer.create(
+      description: '登録テスト', #なくてもOK
+      # email: current_user.email, #なくてもOK
+      card: params['payjp-token'],
+      # metadata: {user_id: current_user.id}
+      metadata: {user_id: 1}
+      ) #念の為metadataにuser_idを入れましたがなくてもOK
 end
