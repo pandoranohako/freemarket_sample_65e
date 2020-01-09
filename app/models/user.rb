669a60010dda5bba4e575c485959baf8b7b4a8e5
itoshin128 <itoshin128@gmail.com>
2020-01-09 17:26:06 +0900

    userモデルにomniauthを使えるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook google_oauth2]
          kanji = /\A[一-龥]+\z/
          kana = /\A([ァ-ン]|ー)+\z/
 
