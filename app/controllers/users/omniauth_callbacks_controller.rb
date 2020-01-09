9ce200f2649c67c785f50aea61e59b3c14649c53
itoshin128 <itoshin128@gmail.com>
2020-01-09 19:21:37 +0900

    ビューのリンク修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,57 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
 
   def facebook
-    callback_for(:facebook)
+    authorization
   end
 
   def google_oauth2
-    callback_for(:google)
-  end
-
-
-  def callback_for(provider)
-    @omniauth = request.env['omniauth.auth']
-    info = User.find_oauth(@omniauth)
-    @user = info[:user]
-    if @user.persisted? 
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-    else 
-      @sns = info[:sns]
-      render template: "devise/registrations/new" 
-    end
+    authorization
   end
 
   def failure
-    redirect_to root_path and return
+    redirect_to root_path
   end
+
+  private
+
+  def authorization
+    sns_info = User.from_omniauth(request.env["omniauth.auth"])
+    @user = sns_info[:user]
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+    else
+      @sns_id = sns_info[:sns].id
+      render template: 'devise/registrations/new'
+    end
+  end
+
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,19 +23,19 @@ class User < ApplicationRecord
   validates :birthday_dd, presence: true
 
 #SNS認証 
-def self.from_omniauth(auth)
-  sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-  # sns認証したことがあればアソシエーションで取得
-  # 無ければemailでユーザー検索して取得orビルド(保存はしない)
-  user = sns.user || User.where(email: auth.info.email).first_or_initialize(
-    nickname: auth.info.name,
-      email: auth.info.email
-  )
-  # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
-  if user.persisted?
-    sns.user = user
-    sns.save
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+        name: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
-  { user: user, sns: sns }
-end
 end
