d88d1f72df9766499d5844ecfa162a3d46b96873
akki0083 <fighters.nkt@gmail.com>
2020-01-09 22:13:15 +0900

    購入確認のアクションを定義

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,2 +1,21 @@
 class PurchaseController < ApplicationController
+
+  require 'payjp'
+
+  def index
+    # card = Card.where(user_id: current_user.id).first
+    card = Card.where(user_id: 1).first
+
+    if card.blank?
+      #登録された情報がない場合にカード登録画面に移動
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key)
+      #保管した顧客IDでpayjpから情報取得
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+
 end
