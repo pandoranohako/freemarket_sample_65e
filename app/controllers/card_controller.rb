1f6f9a1c46fd5c396975ba8baa87bc281a1723bd
akki0083 <fighters.nkt@gmail.com>
2020-01-09 22:04:33 +0900

    newアクションを定義
    userがすでにカードを登録しているときは、カード詳細ページへリダイレクト

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,2 +1,10 @@
 class CardController < ApplicationController
+
+
+  require "payjp"
+
+  def new
+    card = Card.where(user_id: 1) #1をcurrent_user.idとしたい
+    redirect_to action: "show" if card.exists?
+  end
 end
