b8b2b6133425e489010ed76dd2ae7b5f88af3014
akki0083 <fighters.nkt@gmail.com>
2020-01-10 13:27:14 +0900

    purchaseのルーティングをprodctsに入れ子
    purchaceコントローラーでproductsテーブルの情報を使いたいため

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #マイページ
   root "products#index"
-  resources :products, only: [:new] 
+  resources :products, only: [:new, :show] do
+    resources :purchase, only: [:index] do
+      collection do
+        get 'index', to: 'purchase#index'
+        post 'pay', to: 'purchase#pay'
+        get 'done', to: 'purchase#done'
+      end
+    end
+end
 
   resources :card, only: [:new, :show] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
-    end
-  end
-
-  resources :purchase, only: [:index] do
-    collection do
-      get 'index', to: 'purchase#index'
-      post 'pay', to: 'purchase#pay'
-      get 'done', to: 'purchase#done'
     end
   end
 
