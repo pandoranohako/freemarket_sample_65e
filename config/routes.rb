d8dcb7feaa897dff193bfb7897b84e789c551a12
itoshin128 <itoshin128@gmail.com>
2020-01-12 10:32:41 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     get 'signup', to: 'users/registrations#index'
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
+    post 'card',      to: 'users/registrations#create_card'
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
