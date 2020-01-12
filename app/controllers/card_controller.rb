ba29ff531efcb00b2e1dc41b43cc4b54c4b82ab4
itoshin128 <itoshin128@gmail.com>
2020-01-12 15:55:11 +0900

    Update card_controller.rb

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -14,9 +14,9 @@ class CardController < ApplicationController
     else
       customer = Payjp::Customer.create(
       description: '登録テスト', #なくてもOK
-      # email: current_user.email, #なくてもOK
+      email: current_user.email, #なくてもOK
       card: params['payjp-token'],
-      # metadata: {user_id: current_user.id}
+      metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       
