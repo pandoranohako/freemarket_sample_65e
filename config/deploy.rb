270106739ec6f0da0955e4595decb437bb0996ed
akki0083 <fighters.nkt@gmail.com>
2020-01-12 17:54:17 +0900

    deploy設定ファイルをコメントアウト

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,56 +1,56 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.11.2'
+# # config valid only for current version of Capistrano
+# # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+# lock '3.11.2'
 
-# Capistranoのログの表示に利用する
-set :application, 'freemarket_sample_65e'
+# # Capistranoのログの表示に利用する
+# set :application, 'freemarket_sample_65e'
 
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:pandoranohako/freemarket_sample_65e.git'
+# # どのリポジトリからアプリをpullするかを指定する
+# set :repo_url,  'git@github.com:pandoranohako/freemarket_sample_65e.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+# # バージョンが変わっても共通で参照するディレクトリを指定
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+# set :rbenv_type, :user
+# set :rbenv_ruby, '2.5.1'
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/freemarket_sample_65e.pem'] 
+# # どの公開鍵を利用してデプロイするか
+# set :ssh_options, auth_methods: ['publickey'],
+#                   keys: ['~/.ssh/FreemarketSample65e.pem'] 
 
-# プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+# # プロセス番号を記載したファイルの場所
+# set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
+# # Unicornの設定ファイルの場所
+# set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+# set :keep_releases, 5
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# }
 
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+# set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
+# # デプロイ処理が終わった後、Unicornを再起動するための記述
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
 
-  desc 'upload master.key'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
-end
+#   desc 'upload master.key'
+#   task :upload do
+#     on roles(:app) do |host|
+#       if test "[ ! -d #{shared_path}/config ]"
+#         execute "mkdir -p #{shared_path}/config"
+#       end
+#       upload!('config/master.key', "#{shared_path}/config/master.key")
+#     end
+#   end
+#   before :starting, 'deploy:upload'
+#   after :finishing, 'deploy:cleanup'
+# end
