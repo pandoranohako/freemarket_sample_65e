3200f035a4c1363a2447b219a4ba9b4d87205ecf
akki0083 <fighters.nkt@gmail.com>
2020-01-15 12:39:44 +0900

    カード新規登録のアクションを定義

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -6,9 +6,30 @@ class MypageController < ApplicationController
   end
 
   def telephone
+  def card_new
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "card_show" if card.exists?
   end
 
   def address
+  def card_create #payjpとCardのデータベース作成を実施します
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key) #creddential.yml.enb から呼び出せないので仮置き
+    if params['payjp-token'].blank?
+      redirect_to action: "card_new"
+    else
+      customer = Payjp::Customer.create(
+      description: '登録テスト', #なくてもOK
+      card: params['payjp-token'],
+      metadata: {user_id: current_user.id}
+      ) #念の為metadataにuser_idを入れましたがなくてもOK
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      
+      if @card.save
+        redirect_to action: "card_show"
+      else
+        redirect_to action: "card_new"
+      end
+    end
   end
 
   def payment
