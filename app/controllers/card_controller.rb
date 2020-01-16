c816c81466b143ee43e8dd0c680030d5152a7ca0
akki0083 <fighters.nkt@gmail.com>
2020-01-17 08:04:48 +0900

    redirect_toをprefix_path表記に修正

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -6,14 +6,25 @@ class CardController < ApplicationController
 
   def new
     card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+    if card.present?
+      redirect_to card_index_path
+    end
+  end
+
   def index #Cardのデータpayjpに送り情報を取り出します
+    if @card.blank?
+      redirect_to new_card_path
+    else
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
   end
 
   def pay #payjpとCardのデータベース作成を実施します
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key) #creddential.yml.enb から呼び出せないので仮置き
     if params['payjp-token'].blank?
-      redirect_to action: "new"
+      redirect_to new_card_path
     else
       customer = Payjp::Customer.create(
       description: '登録テスト', #なくてもOK
@@ -23,35 +34,24 @@ class CardController < ApplicationController
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       
       if @card.save
-        redirect_to action: "show"
+        redirect_to card_index_path
       else
-        redirect_to action: "new"
+        redirect_to new_card_path
       end
     end
   end
 
-  def show #Cardのデータpayjpに送り情報を取り出します
-    if @card.blank?
-      redirect_to action: "new" 
-    else
-      Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key)
-      customer = Payjp::Customer.retrieve(@card.customer_id)
-      @default_card_information = customer.cards.retrieve(@card.card_id)
-    end
-  end
-
   def delete #PayjpとCardデータベースを削除します
-    unless @card.blank?
+    if @card.present?
       Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key)
       customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
       @card.delete
     end
-      redirect_to action: "new"
+    redirect_to new_card_path
   end
 
   private
-
   def set_card
     @card = Card.find_by(user_id: current_user.id)
   end
