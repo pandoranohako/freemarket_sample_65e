6e13ab56f4522ac9b01aee370c0da83efb52c3ef
akki0083 <fighters.nkt@gmail.com>
2020-01-19 11:54:52 +0900

    コメントアウトを外して、payjpのカード情報を参照できるようにした

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -12,9 +12,9 @@ class PurchaseController < ApplicationController
     else
       Payjp.api_key = Rails.application.credentials.dig(:payjp, :payjp_test_secret_access_key)
       #保管した顧客IDでpayjpから情報取得
-      # customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      # @default_card_information = customer.cards.retrieve(card.card_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
     end
   end
 
