ab7f712b742fc0780498d372368bcdd088ca9c3b
pandoranohako <m.o_5963@icloud.com>
2020-01-22 12:35:46 +0900

    Update product.rb
    sizeとbrandの記述の削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,6 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :category, foreign_key: 'category_id', optional: true
-  belongs_to :size, foreign_key: 'size_id', optional: true
-  belongs_to :brand, foreign_key: 'brand_id', optional: true
+ 
   
 end
